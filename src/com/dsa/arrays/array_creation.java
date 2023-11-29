85d81efc92c40a845f2000c5a06432f0832dd41e
utsaxena <utsaxena@LIN-5CG1347HBL.corp.capgemini.com>
2023-11-29 19:50:18 +0530

    DSA Arrays

---
+++ src/com/dsa/arrays/array_creation.java
@@ -0,0 +1,45 @@
+package com.dsa.arrays;
+
+import java.util.Scanner;
+
+// program to add an element at a particular index and iterate the whole array then
+public class array_creation {
+
+    public static void main(String[] args) {
+
+        int indexOfNewElement, valueOfNewElement;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the elements of the array separated by commas: ");
+        String input = scanner.nextLine();
+
+        String[] elements = input.split(",");
+
+        int a[] = new int[elements.length]; //original array
+        int b[] = new int[elements.length + 1]; // new array
+
+        for (int i = 0; i < elements.length; i++) {
+            a[i] = Integer.parseInt(elements[i]);  //a[0],a[1]
+        }
+
+        System.out.println("Enter index of new value to be inserted : ");
+        indexOfNewElement = scanner.nextInt();  //2
+        System.out.println("Enter the value to be inserted :");
+        valueOfNewElement = scanner.nextInt();
+        for (int i = 0; i < elements.length + 1; i++) {    //10 20 30 40 50
+            if (i < indexOfNewElement) {    //10,20
+                b[i] = a[i];
+            } else if (i == indexOfNewElement) {
+                b[i] = valueOfNewElement;
+            } else {
+                b[i] = a[i-1];
+            }
+        }
+
+        System.out.println("The elements are:");
+        for (int i = 0; i < a.length + 1; i++) {
+            System.out.println(b[i]);
+        }
+
+
+    }
+}
---
+++ src/com/dsa/arrays/array_deletion.java
@@ -0,0 +1,42 @@
+package com.dsa.arrays;
+
+import java.util.Scanner;
+
+public class array_deletion {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int noOfElements,indexOfDeletedElement;
+        System.out.println("Enter the number of elements:");
+        noOfElements=scanner.nextInt();
+
+        int a[] = new int[noOfElements];
+        int b[] = new int[noOfElements-1];
+        System.out.println("Enter the elements of the array:");
+        for(int i= 0;i<noOfElements;i++){
+
+            a[i]=scanner.nextInt();
+        }
+        System.out.println("Enter the  index of element to be deleted:");
+        indexOfDeletedElement= scanner.nextInt();
+        for(int i=0;i< noOfElements;i++){  // a[] =10 20 30 40 , indexOfDeletedElement =2
+            if(i<indexOfDeletedElement){        //b[]=10 20
+                b[i]=a[i];
+            }
+            else if( i== indexOfDeletedElement){
+                continue;
+            }
+            else {
+                b[i-1]=a[i];
+                // if i matches the element to be deleted we skip that element so array b is one step behind than a
+            }
+        }
+        System.out.println("The elements are:");
+        for (int i = 0; i < noOfElements-1; i++) {
+            System.out.println(b[i]);
+        }
+
+
+    }
+}
